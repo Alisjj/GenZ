c5846e5c506500e5c87ec98c9c9466f0414e5213
sjj <aliyusajjads@gmail.com>
2024-03-27 11:49:30 +0100

    update: split delimeter function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,14 @@
 import unittest
 
-from textnode import TextNode
+from textnode import (
+    TextNode,
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+    text_type_bold,
+    text_type_code,
+    text_type_text,
+)
 
 
 class TestTextNode(unittest.TestCase):
@@ -19,6 +27,63 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold", "")
         self.assertEqual(node.url, None)
         self.assertNotEqual(node2.url, None)
+
+    def test_split_nodes_delimeter(self):
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        node2 = TextNode("This is text with a *bold text* in it", text_type_text)
+        node3 = TextNode("This is *text with* a *bold text* in it", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        new_nodes2 = split_nodes_delimiter([node2], "*", text_type_bold)
+        new_nodes3 = split_nodes_delimiter([node3], "*", text_type_bold)
+
+        self.assertEqual(
+            new_nodes,
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" word", text_type_text),
+            ],
+        )
+
+        self.assertEqual(
+            new_nodes2,
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("bold text", text_type_bold),
+                TextNode(" in it", text_type_text),
+            ],
+        )
+
+        self.assertEqual(
+            new_nodes3,
+            [
+                TextNode("This is ", text_type_text),
+                TextNode("text with", text_type_bold),
+                TextNode(" a ", text_type_text),
+                TextNode("bold text", text_type_bold),
+                TextNode(" in it", text_type_text),
+            ],
+        )
+
+    def test_markdown_extraction(self):
+        text = "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/dfsdkjfd.png)"
+        text2 = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+
+        self.assertEqual(
+            extract_markdown_images(text),
+            [
+                ("image", "https://i.imgur.com/zjjcJKZ.png"),
+                ("another", "https://i.imgur.com/dfsdkjfd.png"),
+            ],
+        )
+
+        self.assertEqual(
+            extract_markdown_links(text2),
+            [
+                ("link", "https://www.example.com"),
+                ("another", "https://www.example.com/another"),
+            ],
+        )
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,15 @@
+import re
+
+from htmlnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -15,3 +27,54 @@ class TextNode:
 
     def __repr__(self) -> str:
         return repr(f"TextNode({self.text}, {self.text_type}, {self.url})")
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    result = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            result.append(node)
+            continue
+
+        text: str = node.text
+        start_index = text.find(delimiter)
+        while start_index != -1:
+            closing = text.find(delimiter, start_index + len(delimiter))
+            if closing == -1:
+                raise ValueError(
+                    "Invalid Markdown syntax: Missing closing delimiter for code block."
+                )
+
+            result.append(TextNode(text[:start_index], text_type_text))
+            result.append(TextNode(text[start_index + len(delimiter): closing], text_type))
+
+            text = text[closing + len(delimiter):]
+            start_index = text.find(delimiter)
+        
+        result.append(TextNode(text, text_type_text))
+
+    return result
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
